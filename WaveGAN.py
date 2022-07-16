49db2c7aa1ec2a5e00efad2313a0c7f828be3306
Dev Parikh <47342287+devparikh@users.noreply.github.com>
2022-07-16 16:17:38 -0400

    Update WaveGAN.py

--- WaveGAN.py
+++ WaveGAN.py
@@ -9,8 +9,6 @@ kernel_len = 25
 stride = 4
 channels = 1
 
-latent_dim = 100
-
 def Generator(input_vector):
   # Initialize the model
   model = Sequential()
@@ -78,3 +76,56 @@ def Discriminator(input_data):
 
   # Output(Dense) layer
   model.add(Dense(256 * model_dim))
+
+'''Training WaveGAN'''
+
+# Training Parameters
+batch_size = 64
+latent_dim = 100
+epochs = 50
+
+D_updates = 5
+
+# Training Process
+for iteration in range(1, epochs+1):
+
+  # Training Discriminator
+  for discriminator_updates in range(D_updates):
+
+    # Getting random noise vectors in a array of the batch size from a uniform distribution between -1 and 1
+    noise_vector = tf.random.uniform([batch_size, latent_dim], minval=-1, maxval=1, dtype=tf.float32)
+
+    with tf.name_scope("D_G_Output"), tf.variable_scope("D"):
+      # Getting output from the Discriminator from the generated data from Generator
+      D_G_Output = Discriminator(Generator(noise_vector))
+
+    with tf.name_scope("D_X"), tf.variable_scope("D")
+      # Getting output from the Discriminator for real data
+      D_X = Discriminator(audio_dataset)
+    
+    # Getting variables that I want to control when training
+    D_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='D')
+
+  with tf.name_scope("G_O"), tf.variable_scope("G"):
+    # Getting output from Generator on noise vector
+    G_O = Generator(noise_vector)
+    
+  # Getting key training variables for Generator model
+  Generator_Variables = tf.get_collection([tf.GraphKeys.TRAINABLE_VARIABLES], "G")
+
+
+  # Passing the data through WGAN-GP to get values for generator and discriminator loss
+  Discriminator_Loss, Generator_Loss = WGAN_GP(D_G_Output, D_X, G_O, batch_size)
+
+  # Defining Discriminator Optimizer
+  Discriminator_Optimizer = tf.keras.optimizers.Adam(
+          learning_rate=1e-4,
+          beta1=0.5,
+          beta2=0.9)
+  
+  
+  # Defining Generator Optimizer
+  Generator_Optimizer = tf.keras.optimizers.Adam(
+          learning_rate=1e-4,
+          beta1=0.5,
+          beta2=0.9)
