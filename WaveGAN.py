096c7af1297d0e07e8aabcc310eeb328467b6a94
Dev Parikh <47342287+devparikh@users.noreply.github.com>
2022-07-19 11:25:00 -0400

    Update WaveGAN.py

--- WaveGAN.py
+++ WaveGAN.py
@@ -13,12 +13,12 @@ def Generator(input_vector):
   # Initialize the model
   model = Sequential()
   # Input for the Generator
-  model.add(Input(input_vector))
+  model.add(Input(tensor=input_vector, batch_size=64))
   
   # First Layer
   model.add(Dense(256 * model_dim))
   # Reshape Layer
-  model.add(Reshape(16, 16 * model_dim))
+  model.add(Reshape(batch_size, [16, 16 * model_dim]))
   model.add(Activation("relu"))
 
   # Transposed Conv1D + ReLU
@@ -44,32 +44,27 @@ def Generator(input_vector):
 def Discriminator(input_data):
   model = Sequential()
   # Input layer
-  model.add(Input=(input_data))
+  model.add(Input(shape=(4620, 16384), batch_size=64, dtype="float32"))
   
   # Conv1D + LeakyReLU
   model.add(Conv1D(model_dim, kernel_len, 4, channels))
   model.add(LeakyReLU(alpha=0.2))
-  # Implement Phase Shuffle
 
   # Conv1D + LeakyReLU
   model.add(Conv1D(2 * model_dim, kernel_len, 4))
   model.add(LeakyReLU(alpha=0.2))
-  # Implement Phase Shuffle
 
   # Conv1D + LeakyReLU
   model.add(Conv1D(4 * model_dim, kernel_len, 4))
   model.add(LeakyReLU(alpha=0.2))
-  # Implement Phase Shuffle
 
   # Conv1D + LeakyReLU
   model.add(Conv1D(8 * model_dim, kernel_len, 4))
   model.add(LeakyReLU(alpha=0.2))
-  # Implement Phase Shuffle
 
   # Conv1D + LeakyReLU
   model.add(Conv1D(16 * model_dim, kernel_len, 4))
   model.add(LeakyReLU(alpha=0.2))
-  # Implement Phase Shuffle
 
   # Reshape layer
   model.add(Reshape(256 * model_dim))
@@ -86,36 +81,41 @@ epochs = 50
 
 D_updates = 5
 
+i = 0
+batch_set = []
+batch_length = 64
+
 # Training Process
 for iteration in range(1, epochs+1):
-
+  # Batching the audio dataset
   # Training Discriminator
   for discriminator_updates in range(D_updates):
-
     # Getting random noise vectors in a array of the batch size from a uniform distribution between -1 and 1
-    noise_vector = tf.random.uniform([batch_size, latent_dim], minval=-1, maxval=1, dtype=tf.float32)
+    noise_vector = tf.random.uniform([batch_size, latent_dim], -1., 1., dtype=tf.float32)
 
-    with tf.name_scope("D_G_Output"), tf.variable_scope("D"):
+    with tf.name_scope("D_G_Output"):
       # Getting output from the Discriminator from the generated data from Generator
-      D_G_Output = Discriminator(Generator(noise_vector))
+      G_O = Generator(noise_vector)
+      D_G_Output = Discriminator(G_O)
 
-    with tf.name_scope("D_X"), tf.variable_scope("D")
+    with tf.name_scope("D_X"):
       # Getting output from the Discriminator for real data
       D_X = Discriminator(audio_dataset)
     
     # Getting variables that I want to control when training
-    D_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='D')
+    D_fake_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope="D_G_Output")
+    D_real_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope="D_X")
 
-  with tf.name_scope("G_O"), tf.variable_scope("G"):
+  # Training Generator
+  with tf.name_scope("G_O"):
     # Getting output from Generator on noise vector
     G_O = Generator(noise_vector)
     
   # Getting key training variables for Generator model
-  Generator_Variables = tf.get_collection([tf.GraphKeys.TRAINABLE_VARIABLES], "G")
-
+  G_vars = tf.get_collection([tf.GraphKeys.TRAINABLE_VARIABLES], "G_O")
 
   # Passing the data through WGAN-GP to get values for generator and discriminator loss
-  Discriminator_Loss, Generator_Loss = WGAN_GP(D_G_Output, D_X, G_O, batch_size)
+  WGAN_GP(D_G_Output, D_X, G_O, batch_size)
 
   # Defining Discriminator Optimizer
   Discriminator_Optimizer = tf.keras.optimizers.Adam(
@@ -129,3 +129,6 @@ for iteration in range(1, epochs+1):
           learning_rate=1e-4,
           beta1=0.5,
           beta2=0.9)
+    
+  G_Loss_Min = Generator_Optimizer.minimize(G_loss, var_list=[D_fake_vars, D_real_vars])
+  D_Loss_Min = Generator_Optimizer.minimize(D_loss, var_list=[G_vars])
